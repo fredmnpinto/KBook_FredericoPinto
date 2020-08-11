fdaf482f5dec3b69bd08cf3d0fff0f748c8306f9
Frederico Pinto <59804373+fredmnpinto@users.noreply.github.com>
2020-08-11 14:09:46 +0100

    First Commit
    Finalmente consegui configurar o UiScrollView de forma a de facto fazer o scroll.

---
+++ KBook_FredericoP/KBook_FredericoP/GenreButtonScreen.swift
@@ -0,0 +1,29 @@
+//
+//  GenreButtonScreen.swift
+//  KBook_FredericoP
+//
+//  Created by Alexander on 08/08/20.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
